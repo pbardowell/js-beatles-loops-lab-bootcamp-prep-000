f59a1f335a4e3095380bd377f280ea41bcde2b1d
Patricia Bardowell <patricia.bardowell@gmail.com>
2017-11-01 23:50:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay (musiciansArray, instrumentsArray) {
   var newArray = []
-  for (var i=0; i < 5; i++) {
+  for (var i=0; i < 4; i++) {
     newArray[i] = musiciansArray[i] + " plays " + instrumentsArray[i]
   }
   return newArray
